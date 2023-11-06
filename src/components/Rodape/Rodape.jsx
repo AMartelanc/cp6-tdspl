8604bcd110c076b33cf549ef42c0fde076aeac39
AMartelanc <alemartelanc@gmail.com>
2023-11-06 19:37:08 -0300

    Primeiro Commit + Novos dados gerados

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer className="rodape">
+        <div>
+            <p>Alunos: <span>Alexandre Martelanc - rm550247 e João Vitor Souza Nunes - rm550381</span></p>
+        </div>
+    </footer> 
+  )
+}
+ 
