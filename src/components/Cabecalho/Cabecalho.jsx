65397952ca25f99eb5e6472d9d7e32163fae3354
AMartelanc <alemartelanc@gmail.com>
2023-11-06 23:47:54 -0300

    Links para os views

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -4,6 +4,8 @@ export default function Cabecalho() {
   return (
         <header className="bg-gray-800 text-white h-16">
           <nav className="flex flex-row justify-around">
+            <Link className="link-cabecalho" href="/api-usuarios/get-all-users-view">GET-ALL-USERS</Link>
+            <Link className="link-cabecalho" href="/api-usuarios/get-by-id/view">GET-BY-ID</Link>
             <Link className="link-cabecalho" href="/consumo/eletro-view">ELETRÔNICOS</Link>
             <Link className="link-cabecalho" href="/produtos/estaticas/tenis">TÊNIS</Link>
             <Link className="link-cabecalho" href="/produtos/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link>
